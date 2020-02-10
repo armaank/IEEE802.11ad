5e4ccb59fbea85d66085b077fa8214e01736cecd
armaank <kohli@cooper.edu>
2020-02-10 00:28:57 -0500

    basic structure, added ieee reference .txt files

---
+++ codes.m
@@ -0,0 +1,15 @@
+% defining functions associated with error control coding
+classdef codes
+   methods(Static)
+       
+       function [seq_ldpc] = ldpc(seq, rate)
+           %ldpc encodes a bit sequence
+       end
+       
+       function [seq_golay] = golay(seq)
+           % generates a golay sequence for the gaurd interval
+       end
+       
+   end
+    
+end
---
+++ modulator.m
@@ -0,0 +1,12 @@
+% defining functions associated with modulation
+classdef modulator
+    methods(Static)
+        
+        function [data_bpsk] = bpskmod(data, phase)
+            % bpsk modulator w/ a specified phase offset (typ. pi/2)
+        
+        end
+        % etc, more go here
+    
+    
+end
